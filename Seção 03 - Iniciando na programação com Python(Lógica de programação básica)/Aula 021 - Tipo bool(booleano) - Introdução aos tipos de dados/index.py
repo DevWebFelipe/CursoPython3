33ab503aad92369b7c1332a0e6c429ba714bc38a
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-09-15 19:43:48 -0300

    Aula 021

---
+++ Seção 03 - Iniciando na programação com Python(Lógica de programação básica)/Aula 021 - Tipo bool(booleano) - Introdução aos tipos de dados/index.py
@@ -0,0 +1,13 @@
+#  Tipo de dado bool(boolean)
+#  Ao questionar algo em um programa,
+#  só existem duas respostas possíveis:
+#  sim(true) ou não(false)
+#  Existem vários operadores para "questionar"
+#  Dentre eles o ==, que é um operador lógico que 
+#  questiona se um valor é igual ao outro
+
+print(10 == 10) # SIM = True (Verdadeiro)
+print(10 == 15) # NÃO = False (Falso)
+print(type(True))
+print(type(False))
+print(type(10 == 10), type(10 == 15), sep=', ')
