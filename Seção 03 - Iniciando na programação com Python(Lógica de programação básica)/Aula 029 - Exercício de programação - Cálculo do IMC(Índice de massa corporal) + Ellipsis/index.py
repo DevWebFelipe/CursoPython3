523503c9b5bc0f85b012ae15fcd8e439f6fe388d
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-09-15 23:12:44 -0300

    Aula 029

---
+++ Seção 03 - Iniciando na programação com Python(Lógica de programação básica)/Aula 029 - Exercício de programação - Cálculo do IMC(Índice de massa corporal) + Ellipsis/index.py
@@ -0,0 +1,21 @@
+nome = 'Felipe Texeira'
+altura = 1.75
+peso = 133
+imc = peso / (altura ** 2)
+
+print('Felipe Texeira tem 1.75 de altura')
+print('pesa 133 quilos e seu IMC é:')
+print(imc)
+print()
+
+altura = 1.8
+peso = 95
+imc = peso / (altura ** 2)
+print('Dados do professor para conferência da fórmula')
+print('Altura', altura, 'Peso', peso, sep=' = ') # to colocando sep só pra ir memorizando, mas fica ruim, porque fica com = no meio do valor da altura e da descrição 'Peso'
+print(imc)
+print()
+
+print('Abaixo está o ELLIPSIS')
+print('Se você vai colocar um código aqui, mas ainda não pensou em como fazer isso, pode usar ... no meio do código, não irá gerar nenhum erro (existem outras finalidades)')
+print(...)
