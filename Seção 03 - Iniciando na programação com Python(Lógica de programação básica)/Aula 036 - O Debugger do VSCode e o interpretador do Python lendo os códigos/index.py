49c0417142546c0d2b0b6f12c667e339248bd79d
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-09-16 16:31:11 -0300

    Aula 036

---
+++ Seção 03 - Iniciando na programação com Python(Lógica de programação básica)/Aula 036 - O Debugger do VSCode e o interpretador do Python lendo os códigos/index.py
@@ -0,0 +1,23 @@
+# if / elif      / else
+# se / se não se / então
+
+condicao1 = False
+condicao2 = True
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+  print('Condição 1 é verdadeira')
+elif condicao2:
+  print('Condição 2 é verdadeira')  
+elif condicao3:
+  print('Condição 3 é verdadeira')
+elif condicao4:
+  print('Condição 4 é verdadeira')
+else:
+  print('Nenhuma condição atendida')
+
+if 30 > 10:
+  print('Caiu aqui')  
+
+print('Esse sempre vai fazer')
