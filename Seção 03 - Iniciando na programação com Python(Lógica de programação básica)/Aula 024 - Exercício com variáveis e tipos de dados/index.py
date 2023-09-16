8011e8788bd57ed64badb9ed6f8063d68d3c52f1
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-09-15 21:43:04 -0300

    Aula 024

---
+++ Seção 03 - Iniciando na programação com Python(Lógica de programação básica)/Aula 024 - Exercício com variáveis e tipos de dados/index.py
@@ -0,0 +1,17 @@
+nome = 'Felipe'
+print('Nome:', nome)
+
+sobrenome = 'Texeira'
+print('Sobrenome:', sobrenome)
+
+idade = 28
+print('Idade:', idade)
+
+ano_nascimento = 1994
+print('Ano de nascimento:', ano_nascimento)
+
+maior_de_idade = idade >= 18
+print('É maior de idade:', maior_de_idade)
+
+altura_metros = 1.75
+print('Altura em mretros:', altura_metros)
