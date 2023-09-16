db90c147c9da89e3f857c866e61b0d44f069b6e7
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-09-15 22:35:49 -0300

    Aula 027

---
+++ Seção 03 - Iniciando na programação com Python(Lógica de programação básica)/Aula 027 - Concatenação (+) e repetição (asterísco) com operadores/index.py
@@ -0,0 +1,8 @@
+numero = 5
+concatenacao = 'A' + 'B' + str(1) + str(numero) + 'C' # soma numeros e concatena string
+print(concatenacao)
+
+a_dez_vezes = 'A ' * 10 + str(numero) # se 'multiplicar' uma string, vai repetir a mesma string o numero de vezes que for estipulado
+tres_vezes_felipe = 3 * 'Felipe ' # multiplica numeros e repete strings
+print(a_dez_vezes)
+print(tres_vezes_felipe)
