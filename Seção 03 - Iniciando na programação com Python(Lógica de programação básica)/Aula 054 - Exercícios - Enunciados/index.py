faa5e9ef30455fb46a784524bc1fe3a6229d4d02
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-09-17 15:03:26 -0300

    Adicionei as soluções do professor, só pra ter mais exemplos

--- Seção 03 - Iniciando na programação com Python(Lógica de programação básica)/Aula 054 - Exercícios - Enunciados/index.py
+++ Seção 03 - Iniciando na programação com Python(Lógica de programação básica)/Aula 054 - Exercícios - Enunciados/index.py
@@ -6,7 +6,7 @@ inteiro, informe que não é um número inteiro.
 numero_digitado = input('Digite um numero inteiro: ')
 
 try:
-  numero_digitado = int(numero_digitado)
+  numero_digitado = int(numero_digitado) # dava pra ter usadoo numero_digitado.isdigit() e resolver com IF ao invés de TRY
   if (numero_digitado % 2 == 0):
     print(f'O número "{numero_digitado}" é um número par')
   else:
@@ -14,6 +14,23 @@ try:
 except:
   print('O número digitado não é um número inteiro válido!')
 
+"""
+Solução professor
+
+
+# if entrada.isdigit():
+#     entrada_int = int(entrada)
+#     par_impar = entrada_int % 2 == 0
+#     par_impar_texto = 'ímpar'
+
+#     if par_impar:
+#         par_impar_texto = 'par'
+
+#     print(f'O número {entrada_int} é {par_impar_texto}')
+# else:
+#     print('Você não digitou um número inteiro')
+
+"""
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
 descrito, exiba a saudação apropriada. Ex. 
@@ -34,7 +51,25 @@ try:
       print('Boa noite!')
 except:
   print('O número digitado não é uma hora válida!')
+"""
+Solução professor
 
+entrada = input('Digite a hora em números inteiros: ')
+
+try:
+  hora = int(entrada)
+
+  if hora >= 0 and hora <= 11:
+    print('Bom dia')
+  elif hora >= 12 and hora <= 17:
+    print('Bom tarde')
+  elif hora >= 18 and hora <= 23:
+    print('Bom noite')
+  else:
+    print('Não conheço essa hora')
+except:
+  print('Por favor, digite apenas números inteiros')
+"""
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
@@ -51,3 +86,20 @@ elif nome_normal:
   print('Seu nome é normal')
 else:
   print('Seu nome é muito grande')
+
+"""
+Solução do professor
+
+nome = input('Digite seu nome: ')
+tamanho_nome = len(nome)
+
+if tamanho_nome > 1:
+  if tamanho_nome <= 4:
+    print('Seu nome é curto')
+  elif tamanho_nome >= 5 and tamanho_nome <= 6:
+    print('Seu nome é normal')
+  else:
+    print('Seu nome é muito grande')
+else:
+  print('Digite mais de uma letra.')
+"""
