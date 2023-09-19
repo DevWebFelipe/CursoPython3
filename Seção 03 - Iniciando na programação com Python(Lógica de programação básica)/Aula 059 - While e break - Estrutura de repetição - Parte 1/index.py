4d2c6344af577d90ae98180c5cbc60d774677acc
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-09-19 20:46:49 -0300

    Aula 059

---
+++ Seção 03 - Iniciando na programação com Python(Lógica de programação básica)/Aula 059 - While e break - Estrutura de repetição - Parte 1/index.py
@@ -0,0 +1,22 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+
+"""
+
+condicao = True
+integer = 0
+while condicao:
+  integer = integer + 1
+  print(integer)
+  if integer > 10:
+    condicao = False
+
+condicao = True
+while condicao:
+  nome = input('Qual seu nome? ')  
+  print(f'Seu nome é {nome}')
+  if nome.lower() == 'sair':
+    condicao = False
