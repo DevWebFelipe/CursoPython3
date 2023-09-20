0374c0758b5bab915302d1281bbc8472fa28eebb
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-09-19 21:05:28 -0300

    Aula 060

---
+++ Seção 03 - Iniciando na programação com Python(Lógica de programação básica)/Aula 060 - while - Condição em detalhes/index.py
@@ -0,0 +1,11 @@
+contador = 0
+
+# Contar até 10
+while contador < 10:
+  contador = contador + 1
+  print(contador)  
+
+while False:
+  print('Nunca vai entrar aqui') # fica apagadinho porque Python sabe que nunca vai entrar aqui, porque a condição ta False
+
+print('Acabou os laços')
