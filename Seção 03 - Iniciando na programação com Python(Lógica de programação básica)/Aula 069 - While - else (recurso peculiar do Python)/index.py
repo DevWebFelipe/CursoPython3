0d3ab98824755df353d001eebe175c4225e9a4cb
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-09-21 19:54:45 -0300

    Aula 069

---
+++ Seção 03 - Iniciando na programação com Python(Lógica de programação básica)/Aula 069 - While - else (recurso peculiar do Python)/index.py
@@ -0,0 +1,14 @@
+""" while / else """
+
+string = 'Felipe Texeira'
+
+contador = 0
+
+while contador < len(string):
+  print(string[contador])
+  contador += 1
+else:
+  print('O else foi executado no final do laço')
+  print()
+
+print('Terminou o laço')
