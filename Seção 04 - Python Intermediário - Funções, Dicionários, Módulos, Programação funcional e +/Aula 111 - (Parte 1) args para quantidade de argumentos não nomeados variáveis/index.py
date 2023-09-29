a7f8a720a84cc30bc3bb4abf1f6cd4ff30e212b3
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-09-28 21:22:30 -0300

    Aula 111

---
+++ Seção 04 - Python Intermediário - Funções, Dicionários, Módulos, Programação funcional e +/Aula 111 - (Parte 1) args para quantidade de argumentos não nomeados variáveis/index.py
@@ -0,0 +1,26 @@
+"""
+  args - Argumentos não nomeados
+  * - *args (empacotamento e desenpacotamento)
+
+"""
+
+# Lembre-se de desampacotamento
+
+x, y, * resto = 1, 2, 3, 4
+print(x, y, resto)
+
+def soma(x, y):
+  return x + y
+
+def soma2(*args):
+  total = 0
+  print(type(args), args)
+
+  for numero in args:
+    total += numero
+
+  return total
+
+print(soma2(1,2,3,4,5,6,7))
+
+print(1)
