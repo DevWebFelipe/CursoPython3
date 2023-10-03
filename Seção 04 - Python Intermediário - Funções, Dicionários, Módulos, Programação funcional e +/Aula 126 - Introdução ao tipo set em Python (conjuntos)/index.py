8f40f8a56342a89820d53302e213828d06578bd6
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-10-02 21:18:18 -0300

    Aula 126

---
+++ Seção 04 - Python Intermediário - Funções, Dicionários, Módulos, Programação funcional e +/Aula 126 - Introdução ao tipo set em Python (conjuntos)/index.py
@@ -0,0 +1,40 @@
+# Sets - Conjuntos em Python (tipo set)
+# Conjuntos são ensinados na matemática
+# https://brasilescola.uol.com.br/matematica/conjunto.htm
+# Representados graficamente pelo diagrama de Venn
+# Sets em Python são mutáveis, porém aceitam apenas
+# tipos imutáveis como valor interno.
+
+# Criando um set
+# set(iterável) ou {1, 2, 3}
+# s1 = set('Felipe')
+
+# são parecidos com dicionários, porém não tem par com chave e valor, eles possuem apenas o valor
+
+s1 = set()  # vazio
+print(s1, type(s1))
+print()
+
+s2 = set('Felipe')  # vazio
+print(s2, type(s2))
+print()
+
+s3 = {'Felipe', 1, 2, 3}  # com dados
+print(s3)
+
+# Sets são eficientes para remover valores duplicados
+# de iteráveis.
+# - Não aceitam valores mutáveis;
+# - Seus valores serão sempre únicos;
+# - não tem índexes;
+# - não garantem ordem;
+# - são iteráveis (for, in, not in)
+
+# Métodos úteis:
+# add, update, clear, discard
+
+# Operadores úteis:
+# união | união (union) - Une
+# intersecção & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em ambos
