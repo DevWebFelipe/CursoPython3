dcc3445ee78e4354e774fffc9586902459756451
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-10-09 20:40:33 -0300

    Aula 157

---
+++ Seção 04 - Python Intermediário - Funções, Dicionários, Módulos, Programação funcional e +/Aula 157 - Introdução aos packages (pacotes) em Python/index.py
@@ -0,0 +1,27 @@
+
+import importlib # essa lib permite que o mesmo módulo seja importado mais de uma vez
+
+#      PASTA   MODULO ALIAS APELIDO
+import modulos.modulo as    modulo # dessa maneira, por padrão, o modulo será importado apenas uma vez em todo projeto
+
+from modulos import modulo
+from modulos.modulo import * 
+from modulos.modulo import variavel
+
+print(modulo.variavel)
+print()
+print(variavel)
+print()
+
+for i in range(10):
+    print(i)
+    import modulos.modulo # enquanto singleton, vai importar uma unica vez em todo o sistema
+
+print()
+
+for i in range(10):
+    print(i)
+    importlib.reload(modulos.modulo) # dessa forma, posso reimportar/recarregar o modulo
+
+print('Fim')
+print()
---
+++ Seção 04 - Python Intermediário - Funções, Dicionários, Módulos, Programação funcional e +/Aula 157 - Introdução aos packages (pacotes) em Python/modulos/modulo.py
@@ -0,0 +1,11 @@
+# Posso determinar o que vai estar disponivel na iumportação de tudo
+
+__all__ = [
+    'variavel',
+]
+
+print('Arquivo MODULO.PY importado/atualizado')
+variavel = 'Var do arquivo MODULO.PY'
+
+def soma(x, y):
+    return x + y
